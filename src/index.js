0d0dfef2f84e00489aaeb6c28ac1695b8ac63ddb
nekotog47 <nekotog47@gmail.com>
2020-03-29 18:14:27 +0900

    implement simple html

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const component = () => {
+  const element = document.createElement('div');
+  const array = ['Hello','Webpack'];
+  element.innerHTML = _.join(array, ' ');
+  return element;
+}
+document.body.appendChild(component());
